387c0f7c5519f6fb44065ce9fbe664219eec9c41
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2026-09-06 12:23:54 +0200

    README.md,ptgzip.c: preparing realease v0.7.5
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- ptgzip.c
+++ ptgzip.c
@@ -38,7 +38,7 @@
 #define LICENSE \
     "(c) 2026, Roberto A. Foglietta <roberto.foglietta@gmail.com>, GPL v2"
 
-#define VERSION "ptgzip v0.7.4"
+#define VERSION "ptgzip v0.7.5"
 
 #define ALWAYS_INLINE __attribute__ ((always_inline)) inline
 #define ALIGNED4      __attribute__ ((aligned(4)))
